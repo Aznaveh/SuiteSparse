03621410ebd391a36ca3e34b8c6eb4dce76baee8
Aznaveh <mahmoudi.mohsen@gmail.com>
2017-07-31 12:00:23 -0500

    bringing data need for 4 path

--- ParU/Source/paru_fourPath.cpp
+++ ParU/Source/paru_fourPath.cpp
@@ -4,9 +4,28 @@
 
 #include "Parallel_LU.hpp"
 
-void paru_fourPath (paru_matrix *paruMatInfo){
+void paru_fourPath (paru_matrix *paruMatInfo,
+        Int rowCount,
+        Int colCount){
 
     DEBUGLEVEL(0);
-    
-    PRLEVEL (1, ("Hi\n"));
+    work_struct *Work =  paruMatInfo->Work;
+
+    Int *fsRowList = Work->scratch; // fully summed row list
+    Int *isRowInFront = Work->rowSize; 
+    Int rowMark = Work->rowMark;
+
+    Int *CBColList = Work -> scratch + 2*rowCount; //scratch=[fsRowList..ipiv..]
+    Int *isColInCBcolSet = Work -> colSize;
+    Int colMark = Work -> colMark;
+
+
+    Int *elRow = Work -> elRow; 
+    Int elRMark = Work -> elRMark;
+    Int *elCol = Work -> elCol;
+    Int elCMark = Work -> elCMark;
+
+
+
+    PRLEVEL (0, ("Hi\n"));
 }
