0051ff5993f3b56388ab2160d8c100b2fe61de35
Aznaveh <mahmoudi.mohsen@gmail.com>
2017-11-06 10:35:19 -0600

    releative Row index is 0 based

--- ParU/Source/paru_numeric_assemble.cpp
+++ ParU/Source/paru_numeric_assemble.cpp
@@ -10,11 +10,15 @@
 void assemble_col (double *sC, double *dC,   //source col and destination col
                     Int m, Int *relRowInd)
 {
+    DEBUGLEVEL (1);
     for (Int i = 0; i < m; i++) {
-        if ( relRowInd[i] > 0 )  // If still valid
+        PRLEVEL (1, ("relRowInd [%ld] =%ld\n",i ,relRowInd [i] ));
+        if ( relRowInd[i] >= 0 ) { // If still valid
+            PRLEVEL (1, ("sC [%ld] =%2.5lf\n", i, sC [i]));
             dC [relRowInd[i] ] += sC[i];
-
+        }
     }
+    PRLEVEL (1, ("\n"));
 }
 
 void assemble_row (double *sM, double *dM,//source Matrix and destination matrix 
@@ -24,8 +28,9 @@ void assemble_row (double *sM, double *dM,//source Matrix and destination matrix
                     Int *relColInd)
 //Source and destination are stored column based
 {
+    DEBUGLEVEL (0);
     for (Int i = 0; i < sn; i++) {
-        if ( relColInd[i] > 0 ){  // If still valid
+        if ( relColInd[i] >= 0 ){  // If still valid
             dM [relColInd [i]*dm + dR] += sM [sm*i + sR];
             PRLEVEL (1, ("adding %lf\n", sM [sm*i + sR]));
         }
@@ -39,10 +44,11 @@ void assemble_all (double *s, double *d,   //source and destination
                     Int *relRowInd, Int *relColInd)
 //Source and destination are stored column based
 {
+    DEBUGLEVEL (0);
     for (Int i = 0; i < sn; i++) {
-        if ( relColInd[i] > 0 ){  // If column is valid
+        if ( relColInd[i] >= 0 ){  // If column is valid
             for (Int j = 0; j < sm; j++) {
-                if ( relRowInd[j] > 0 )  // If row is valid
+                if ( relRowInd[j] >= 0 )  // If row is valid
                     d [relColInd[i]*dm + relRowInd[j] ] += s[sm*i + j];
 
             }
