18f79f740bb13f10122d6925bc866e726b37c3f6
Mohsen Azna <aznaveh@csce-j1z5s52.cs.tamu.edu>
2019-10-31 16:16:48 -0500

    for some matrices that way does not work

--- ParU/MATLAB/save_sp_mat.m
+++ ParU/MATLAB/save_sp_mat.m
@@ -1,6 +1,7 @@
 function save_sp_mat(A)
-    [row col v] = find(A);
-    T = [size(A) nnz(A)];
-    T = [T; row col v];
-    dlmwrite('../Matrix/ParUTst/tmp.mtx',T,'delimiter', '\t')
+%    [row col v] = find(A);
+%    T = [size(A) nnz(A)];
+%    T = [T; row col v];
+%    dlmwrite('../Matrix/ParUTst/tmp.mtx',T,'delimiter', '\t')
+mmwrite('../Matrix/ParUTst/tmp.mtx',A)
 
