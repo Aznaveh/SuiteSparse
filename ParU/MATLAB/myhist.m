7cc4c7b3553875f9f5611fde8fc8589f362eb1d0
Mohsen Azna <aznaveh@csce-j1z5s52.cs.tamu.edu>
2020-01-06 10:04:04 -0600

    dgemm hist

--- ParU/MATLAB/myhist.m
+++ ParU/MATLAB/myhist.m
@@ -1,5 +1,7 @@
-T = load ('../Demo/sinc18');
+%T = load ('../Demo/sinc18');
 %T = load ('../Demo/umfSinc18');
+T = load ('../Demo/twoton');
+%T = load ('../Demo/umftwoton');
 M = T(:,1);
 N = T(:,2);
 K = T(:,3);
@@ -9,4 +11,4 @@ for  i = 1:length(K)
     ind = ic (i);
     F(ind) = F(ind)+ M(i)*N(i)*K(i);
 end
-bar(KK, F, 1)
+bar(KK, F, 10)
