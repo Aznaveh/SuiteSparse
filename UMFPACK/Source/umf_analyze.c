b2dc0e88603839e5548a1d846aada448d1b05f01
Tim Davis <davis@tamu.edu>
2020-03-03 12:32:42 -0600

    debug printfs in umf_analyze

--- UMFPACK/Source/umf_analyze.c
+++ UMFPACK/Source/umf_analyze.c
@@ -30,6 +30,16 @@
 #include "umf_apply_order.h"
 #include "umf_fsize.h"
 
+#undef NDEBUG
+#undef DEBUG1
+#undef DEBUG2
+#undef DEBUG3
+#undef ASSERT
+#define DEBUG1(param) printf param
+#define DEBUG2(param) printf param
+#define DEBUG3(param) printf param
+#define ASSERT(param)
+
 /* ========================================================================== */
 
 GLOBAL Int UMF_analyze
