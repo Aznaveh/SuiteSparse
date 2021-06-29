ede3e8c1167dae4416d6e17dbc0d6ef1320ff920
Mohsen Aznaveh <aznaveh@tamu.edu>
2021-06-29 16:51:09 -0500

    Ooops forgotten this too

---
+++ ParU/Demo/paru_demo.cpp
@@ -0,0 +1,124 @@
+/** =========================================================================  /
+ * =======================  paru_demo =======================================  /
+ * ==========================================================================  /
+ * @brief    test to see how to call umfpack symbolic analysis
+ *
+ * @author Aznaveh
+ * */
+#include "ParU.hpp"
+
+int main(int argc, char **argv)
+{
+    cholmod_common Common, *cc;
+    cholmod_sparse *A;
+    paru_symbolic *LUsym = NULL;
+
+    //~~~~~~~~~Reading the input matrix and test if the format is OK~~~~~~~~~~~~
+    // start CHOLMOD
+    cc = &Common;
+    int mtype;
+    cholmod_l_start(cc);
+
+    // A = mread (stdin) ; read in the sparse matrix A
+    A = (cholmod_sparse *)cholmod_l_read_matrix(stdin, 1, &mtype, cc);
+    if (A == NULL)
+    {
+        printf("Paru: input matrix is invalid\n");
+        exit(1);
+    }
+
+    if (mtype != CHOLMOD_SPARSE)
+    {
+        printf("Paru: input matrix must be sparse\n");
+        exit(1);
+    }
+
+    if (A->xtype != CHOLMOD_REAL)
+    {
+        printf("Paru: input matrix must be real\n");
+        exit(1);
+    }
+
+    //~~~~~~~~~~~~~~~~~~~Starting computation~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+    int scale = 0;
+    Int NoProblem = 0;
+
+    double my_start_time = omp_get_wtime();
+
+    LUsym = paru_analyze(A);
+    if (LUsym == NULL)
+    {
+        cholmod_l_free_sparse(&A, cc);
+        cholmod_l_finish(cc);
+        return PARU_INVALID;
+    }
+    ParU_ResultCode info;
+    paru_matrix *paruMatInfo;
+    info = paru_factorize(A, LUsym, &paruMatInfo);
+    double my_time = omp_get_wtime() - my_start_time;
+    if (info != PARU_SUCCESS)
+    {
+        cholmod_l_free_sparse(&A, cc);
+        cholmod_l_finish(cc);
+        return info;
+    }
+
+    //~~~~~~~~~~~~~~~~~~~End computation~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+    //~~~~~~~~~~~~~~~~~~~Calling umfpack~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+#if 1
+    double umf_start_time = omp_get_wtime();
+    double status,           // Info [UMFPACK_STATUS]
+        Info[UMFPACK_INFO],  // Contains statistics about the symbolic analysis
+
+        Control[UMFPACK_CONTROL];  // it is set in umfpack_dl_defaults and
+    // is used in umfpack_dl_symbolic; if
+    // passed NULL it will use the defaults
+    umfpack_dl_defaults(Control);
+    Control[UMFPACK_ORDERING] = UMFPACK_ORDERING_METIS;
+    // Control [UMFPACK_STRATEGY] =   UMFPACK_STRATEGY_UNSYMMETRIC;
+
+    Int *Ap = (Int *)A->p;
+    Int *Ai = (Int *)A->i;
+    double *Ax = (double *)A->x;
+    // Int m = A->nrow;
+    Int n = A->ncol;
+    void *Symbolic, *Numeric;  // Output argument in umf_dl_symbolc;
+
+    status = umfpack_dl_symbolic(n, n, Ap, Ai, Ax, &Symbolic, Control, Info);
+    if (status < 0)
+    {
+        umfpack_dl_report_info(Control, Info);
+        umfpack_dl_report_status(Control, status);
+        printf("umfpack_dl_symbolic failed\n");
+        exit(0);
+    }
+    status = umfpack_dl_numeric(Ap, Ai, Ax, Symbolic, &Numeric, Control, Info);
+    if (status < 0)
+    {
+        umfpack_dl_report_info(Control, Info);
+        umfpack_dl_report_status(Control, status);
+        printf("umfpack_dl_numeric failed\n");
+    }
+
+    double umf_time = omp_get_wtime() - umf_start_time;
+    umfpack_dl_free_symbolic(&Symbolic);
+    umfpack_dl_free_numeric(&Numeric);
+#endif
+
+    // Writing results to a file
+    if (info == PARU_SUCCESS)
+    {
+        paruMatInfo->umf_time = umf_time;
+        paru_write(paruMatInfo, scale, argv[1]);
+    }
+
+    //~~~~~~~~~~~~~~~~~~~Free Everything~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    paru_freemat(&paruMatInfo);
+    paru_freesym(&LUsym);
+
+    cholmod_l_free_sparse(&A, cc);
+    cholmod_l_finish(cc);
+}
