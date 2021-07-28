6c872628d4cc9f271a22a3387b6e3ce82ade0f51
Mohsen Aznaveh <aznaveh@tamu.edu>
2021-07-28 16:39:14 -0500

    from last commit

--- ParU/Source/paru_perm.cpp
+++ ParU/Source/paru_perm.cpp
@@ -15,14 +15,14 @@
  *                         <----q----
  *
  *                          Pfin (COMPUTED HERE)
- *                    ------------------>   
+ *                    ------------------>
  *                      Pinit      oldRofS (paru_write)
- *                    --------> -------->   
+ *                    --------> -------->
  *                   A         S           LU
- *                    <-------   <-------  
+ *                    <-------   <-------
  *         (paru_analyze)Pinv     newRofS (paru_write)
- *                
- * 
+ *
+ *
  *  We need these permuataions for compuing Ax = b
  *        x = b (p)
  *        x = L\x
@@ -40,39 +40,38 @@ void paru_perm(paru_matrix *paruMatInfo)
 {
 
     paru_symbolic *LUsym = paruMatInfo->LUsym;
+
+    if (LUsym->Pfin != NULL) // it must have been computed
+        return;
     Int nf = LUsym->nf;
 
     Int m = LUsym->m;
-    Int n = LUsym->n;
-    Int n1 = LUsym->n1;  // row+col singletons
+    // Int n = LUsym->n;
+    Int n1 = LUsym->n1; // row+col singletons
 
-    paru_fac *LUs = paruMatInfo->partial_LUs;
-    paru_fac *Us = paruMatInfo->partial_Us;
+    // paru_fac *LUs = paruMatInfo->partial_LUs;
+    // paru_fac *Us = paruMatInfo->partial_Us;
     Int *Super = LUsym->Super;
 
     // some working memory that is freed in this function
-    Int *oldRofS = NULL;
-    Int *newRofS = NULL;
+    Int *Pfin = NULL;
     Int *Pinit = LUsym->Pinit;
 
-    oldRofS = (Int *)paru_alloc(m, sizeof(Int));  // S -> LU P
-    newRofS = (Int *)paru_alloc(m, sizeof(Int));  // Pinv of S
+    LUsym->Pfin = Pfin = (Int *)paru_alloc(m, sizeof(Int));
 
-    if (oldRofS == NULL || newRofS == NULL)
+    if (Pfin == NULL)
     {
         printf("memory problem for writing into files\n");
-        paru_free(m, sizeof(Int), oldRofS);
-        paru_free(m, sizeof(Int), newRofS);
         return;
     }
 
-    Int ip = 0;  // number of rows seen so far
+    Int ip = 0; // number of rows seen so far
     for (Int k = 0; k < n1; k++)
         // first singletons
-        //fprintf(rowfptr, "%ld\n", Pinit[k]);
+        Pfin[ip++] = Pinit[k];
 
     for (Int f = 0; f < nf; f++)
-    {  // rows for each front
+    { // rows for each front
         Int col1 = Super[f];
         Int col2 = Super[f + 1];
         Int fp = col2 - col1;
@@ -80,18 +79,14 @@ void paru_perm(paru_matrix *paruMatInfo)
 
         for (Int k = 0; k < fp; k++)
         {
-            oldRofS[ip++] = frowList[k];  // computing permutation for S
             // P[k] = i
-            //fprintf(rowfptr, "%ld\n", Pinit[frowList[k]]);
+            Pfin[ip++] = Pinit[frowList[k]];
         }
     }
-    //fclose(rowfptr);
     PRLEVEL(1, ("%% row permutaion DONE\n"));
 
     //-------- computing the direct permutation of S
-    for (Int k = 0; k < m - n1; k++)
-        // Inv permutation for S Pinv[i] = k;
-        newRofS[oldRofS[k]] = k;
-
-
+    // for (Int k = 0; k < m - n1; k++)
+    // Inv permutation for S Pinv[i] = k;
+    // newRofS[oldRofS[k]] = k;
 }
