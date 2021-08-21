77bcedc76bbccfb272d6af3394779a6e59042399
Aznaveh <mahmoudi.mohsen@gmail.com>
2021-08-21 11:56:20 -0500

    solve in develop. I think I can solve the scale problem without having this fixed

--- ParU/Source/paru_solve.cpp
+++ ParU/Source/paru_solve.cpp
@@ -1,45 +1,58 @@
 /** =========================================================================  /
  * =======================  paru_solve ======================================  /
  * ==========================================================================  /
- * @brief    get a matrix and factorize it
- *      Allocate space for paruMatInfo
- *      the user should free the space
+ * @brief  sovle Ax = b
+ *      get a factorized matrix and a right hand side 
+ *      returns x
  *
  * @author Aznaveh
  * */
 
 #include "paru_internal.hpp"
 
-ParU_ResultCode paru_solve(Int f, paru_matrix *paruMatInfo)
+ParU_ResultCode paru_solve(paru_matrix *paruMatInfo, double *b)
 // This routine call paru_front from first(f)...f including f
 // This routine is called recursively to make tasks
 {
-    // TODO: temporary to test perm
-    paru_perm(paruMatInfo);
+    paru_symbolic *LUsym = paruMatInfo->LUsym;
     Int m = LUsym->m;
-    double b[m];
-    double x[m];
-    double xt[m];
-    for (Int i = 0; i < m; ++i) b[i] = i + 1;
-    paru_apply_perm(LUsym->Pfin, b, x, m);  // x = p (b)
-    if (paruMatInfo->scale_row)
+    // TODO:  this goes to the test
+    //if (LUsym->Pfin != NULL ) paru_perm(paruMatInfo);
+    //
+    //double b[m];
+    //double x[m];
+    //double xt[m];
+    //for (Int i = 0; i < m; ++i) b[i] = i + 1;
+      
+    double *x = (double *)paru_alloc(m, sizeof(double));
+    if ( x == NULL)
+    {
+        printf("Memory problem inside solve\n");
+        return PARU_OUT_OF_MEMORY;
+    }
+      
+      
+    paru_apply_perm(LUsym->Pfin, b, x, m);  // x = b (p)
+    if (paruMatInfo->scale_row) // x = s.Ps[x]
         paru_apply_scale (paruMatInfo->scale_row, LUsym->Ps, x, m, LUsym->n1);
-    paru_lsolve(paruMatInfo, x);
-    paru_usolve(paruMatInfo, x);
-    paru_apply_inv_perm(LUsym->Qfill, x, xt, m);  // xt = qinv (x)
+
+    paru_lsolve(paruMatInfo, x);    // x = L\x 
+    paru_usolve(paruMatInfo, x);    // x = U\x 
+    paru_apply_inv_perm(LUsym->Qfill, x, b, m);  // b(q) = x
 
 
-    printf ("x = [ ");
-    for (Int i = 0; i < MIN(m,10); ++i) 
-        printf ("%lf ",xt[i]);
-    printf (" ...]\n");
+    // This can go to test
+    //printf ("x = [ ");
+    //for (Int i = 0; i < MIN(m,10); ++i) 
+    //    printf ("%lf ",xt[i]);
+    //printf (" ...]\n");
 
-    for (Int i = 0; i < m; ++i) b[i] *= -1;
-    //    b[i] = 0;
-    paru_gaxpy(A, xt, b);
-    double res = paru_vec_1norm(b, m);
-    double weighted_res = res / (paru_spm_1norm(A) * paru_vec_1norm(xt, m));
-    printf("Residual is |%.2lf| and weigheted residual is |%.2f|.\n", 
-            log10(res), log10(weighted_res) );
+    //for (Int i = 0; i < m; ++i) b[i] *= -1;
+    ////    b[i] = 0;
+    //paru_gaxpy(A, xt, b);
+    //double res = paru_vec_1norm(b, m);
+    //double weighted_res = res / (paru_spm_1norm(A) * paru_vec_1norm(xt, m));
+    //printf("Residual is |%.2lf| and weigheted residual is |%.2f|.\n", 
+    //        log10(res), log10(weighted_res) );
 
 }
