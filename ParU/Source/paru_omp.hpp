09123429cf8ca3bfd99f9ba29a49243b965e36ba
Aznaveh <aznaveh@tamu.edu>
2022-03-24 11:03:54 -0500

    adding other needed in debug mode omp modules

--- ParU/Source/paru_omp.hpp
+++ ParU/Source/paru_omp.hpp
@@ -16,6 +16,8 @@
     #define PARU_OPENMP_GET_THREAD_ID     omp_get_thread_num ( )
     #define PARU_OPENMP_SET_DYNAMIC(d)   omp_set_dynamic(d)
     #define PARU_OPENMP_SET_MAX_ACTIVE_LEVELS(l)   omp_set_max_active_levels(l)
+    #define PARU_OPENMP_GET_ACTIVE_LEVEL   omp_get_active_level()
+    #define PARU_OPENMP_GET_THREAD_NUM   omp_get_thread_num ( )
 
 #else
 
@@ -25,6 +27,8 @@
     #define PARU_OPENMP_GET_THREAD_ID     (0)
     #define PARU_OPENMP_SET_DYNAMIC       (0)
     #define PARU_OPENMP_SET_MAX_ACTIVE_LEVELS(l)   ()
+    #define PARU_OPENMP_GET_ACTIVE_LEVEL   (0)
+    #define PARU_OPENMP_GET_THREAD_NUM     (0)
 #endif
 
 #endif
