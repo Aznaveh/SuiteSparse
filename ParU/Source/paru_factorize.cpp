52b2f93dac2f718586e21260227cdf7f8356bd88
Aznaveh <aznaveh@tamu.edu>
2022-04-13 12:20:51 -0500

    Testing shows acceptable results on parallel for reduction; It would be cumbersome to implement it during factorization and I am not sure how much gain would it have. leave it for now

--- ParU/Source/paru_factorize.cpp
+++ ParU/Source/paru_factorize.cpp
@@ -462,7 +462,8 @@ ParU_Ret ParU_Factorize(cholmod_sparse *A, ParU_Symbolic *Sym,
         else 
         { //Parallel
             Int *Super = Sym->Super;
-            #pragma omp parallel for reduction(max:max_rc) reduction(max: max_cc) if (nf > 65536)
+            #pragma omp parallel for reduction(max:max_rc) \
+            reduction(max: max_cc) if (nf > 65536)
             for (Int f = 0; f < nf; f++)
             {
                 Int rowCount = Num->frowCount[f];
@@ -481,7 +482,8 @@ ParU_Ret ParU_Factorize(cholmod_sparse *A, ParU_Symbolic *Sym,
                 Int col2 = Super[f + 1];
                 Int fp = col2 - col1;
                 double *A = LUs[f].p;
-                #pragma omp parallel for reduction(min:min_udiag) reduction(max: max_udiag) 
+                #pragma omp parallel for reduction(min:min_udiag)\
+                reduction(max: max_udiag) 
                 for (Int i = 0; i < fp; i++)
                 {
                     double udiag = fabs(A[rowCount * i + i]);
