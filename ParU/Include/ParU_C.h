9d344aef71f411a2fd5d5fa0d2fb238197d53c70
Aznaveh <aznaveh@tamu.edu>
2022-10-31 13:06:01 -0500

    adding all the needed data structure

--- ParU/Include/ParU_C.h
+++ ParU/Include/ParU_C.h
@@ -23,6 +23,8 @@ extern "C" {
 // =============================================================================
 // ========================= ParU_C_Control ====================================
 // =============================================================================
+// Just like ParU_Control the only difference is the initialization whic is 
+// handled in the C interface
 typedef struct ParU_C_Control_struct
 {
     Int mem_chunk;  // chunk size for memset and memcpy
@@ -50,6 +52,26 @@ typedef struct ParU_C_Control_struct
     // if the user do not provide a smaller number
 } ParU_C_Control;
 
+// =============================================================================
+// ========================= ParU_C_Symbolic ===================================
+// =============================================================================
+// just a carrier for the C++ data structure
+typedef struct ParU_C_Symbolic_struct
+{
+    void *sym_handle;
+} ParU_C_Symbolic;
+
+// =============================================================================
+// ========================= ParU_C_Numeric ====================================
+// =============================================================================
+// just a carrier for the C++ data structure
+typedef struct ParU_C_Numeric_struct
+{
+    void *num_handle;
+} ParU_C_Numeric;
+
+
+
 // ParU_Version: 
 // print out the version
 //------------------------------------------------------------------------------
@@ -64,20 +86,20 @@ ParU_Ret ParU_C_Analyze(
         // input:
         cholmod_sparse *A,  // input matrix to analyze ...
         // output:
-        ParU_Symbolic **Sym_handle,  // output, symbolic analysis
+        ParU_C_Symbolic **Sym_handle,  // output, symbolic analysis
         // control:
         ParU_C_Control *Control);
 
 //------------------------------------------------------------------------------
 // ParU_Factorize: Numeric factorization is done in this routine. Scaling and
-// making Sx matrix, computing factors and permutations is here. ParU_Symbolic
+// making Sx matrix, computing factors and permutations is here. ParU_C_Symbolic
 // structure is computed ParU_Analyze and is an input in this routine.
 //------------------------------------------------------------------------------
 ParU_Ret ParU_C_Factorize(
         // input:
-        cholmod_sparse *A, ParU_Symbolic *Sym,
+        cholmod_sparse *A, ParU_C_Symbolic *Sym,
         // output:
-        ParU_Numeric **Num_handle,
+        ParU_C_Numeric **Num_handle,
         // control:
     ParU_C_Control *Control);
 
@@ -89,7 +111,7 @@ ParU_Ret ParU_C_Factorize(
 //-------- Ax = b (x is overwritten on b)---------------------------------------
 ParU_Ret ParU_C_Solve_axx(
     // input:
-    ParU_Symbolic *Sym, ParU_Numeric *Num,
+    ParU_C_Symbolic *Sym, ParU_C_Numeric *Num,
     // input/output:
     double *b,
     // control:
@@ -97,7 +119,7 @@ ParU_Ret ParU_C_Solve_axx(
 //-------- Ax = b --------------------------------------------------------------
 ParU_Ret ParU_C_Solve_axb(
     // input:
-    ParU_Symbolic *Sym, ParU_Numeric *Num, double *b,
+    ParU_C_Symbolic *Sym, ParU_C_Numeric *Num, double *b,
     // output
     double *x,
     // control:
@@ -105,7 +127,7 @@ ParU_Ret ParU_C_Solve_axb(
 //-------- AX = B  (X is overwritten on B, multiple rhs)------------------------
 ParU_Ret ParU_C_Solve_AXX(
     // input
-    ParU_Symbolic *Sym, ParU_Numeric *Num, Int nrhs,
+    ParU_C_Symbolic *Sym, ParU_C_Numeric *Num, Int nrhs,
     // input/output:
     double *B,  // m(num_rows of A) x nrhs
     // control:
@@ -113,7 +135,7 @@ ParU_Ret ParU_C_Solve_AXX(
 //-------- AX = B  (multiple rhs)-----------------------------------------------
 ParU_Ret ParU_C_Solve_AXB(
     // input
-    ParU_Symbolic *Sym, ParU_Numeric *Num, Int nrhs, double *B,
+    ParU_C_Symbolic *Sym, ParU_C_Numeric *Num, Int nrhs, double *B,
     // output:
     double *X,
     // control:
@@ -144,9 +166,9 @@ ParU_Ret ParU_C_Residual_BAX(
 //------------------------------------------------------------------------------
 //------------ Free routines----------------------------------------------------
 //------------------------------------------------------------------------------
-ParU_Ret ParU_C_Freenum(ParU_Numeric **Num_handle, ParU_C_Control *Control);
+ParU_Ret ParU_C_Freenum(ParU_C_Numeric **Num_handle, ParU_C_Control *Control);
 
-ParU_Ret ParU_C_Freesym(ParU_Symbolic **Sym_handle, ParU_C_Control *Control);
+ParU_Ret ParU_C_Freesym(ParU_C_Symbolic **Sym_handle, ParU_C_Control *Control);
 
 #ifdef __cplusplus
 }
