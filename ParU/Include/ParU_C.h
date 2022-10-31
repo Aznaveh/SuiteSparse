7f28a04b0392626bf9bfa07e812ff483c81441ca
Aznaveh <aznaveh@tamu.edu>
2022-10-31 12:31:06 -0500

    adding the control data structure

--- ParU/Include/ParU_C.h
+++ ParU/Include/ParU_C.h
@@ -4,7 +4,7 @@
 // TODO: I am not sure if I should have a defintion file or not
 // but it might be a good idea
 //
-// I am not sure how to initialize ParU_Control and even how to control it in C
+// I am not sure how to initialize ParU_C_Control and even how to control it in C
 // Unsure how to control ParU_Ret
 //
 //
@@ -14,7 +14,42 @@
 #ifndef PARU_C_H
 #define PARU_C_H
 
+#ifdef __cplusplus
 extern "C" {
+#endif 
+
+
+
+// =============================================================================
+// ========================= ParU_C_Control ====================================
+// =============================================================================
+typedef struct ParU_C_Control_struct
+{
+    Int mem_chunk;  // chunk size for memset and memcpy
+
+    // Symbolic controls
+    Int umfpack_ordering;
+    Int umfpack_strategy;  // symmetric or unsymmetric
+    Int umfpack_default_singleton; //filter singletons if true
+
+    Int relaxed_amalgamation_threshold;
+    // symbolic analysis tries that each front have more pivot columns
+    // than this threshold
+
+    // Numeric controls
+    Int scale;         // if 1 matrix will be scaled using max_row
+    Int panel_width;  // width of panel for dense factorizaiton
+    Int paru_strategy;  // the same strategy umfpack used
+
+    double piv_toler;     // tolerance for accepting sparse pivots
+    double diag_toler;  // tolerance for accepting symmetric pivots
+    Int trivial;  // dgemms with sizes less than trivial doesn't call BLAS
+    Int worthwhile_dgemm;  // dgemms bigger than worthwhile are tasked
+    Int worthwhile_trsm;  // trsm bigger than worthwhile are tasked
+    Int paru_max_threads;    // It will be initialized with omp_max_threads
+    // if the user do not provide a smaller number
+} ParU_C_Control;
+
 // ParU_Version: 
 // print out the version
 //------------------------------------------------------------------------------
@@ -31,7 +66,7 @@ ParU_Ret ParU_C_Analyze(
         // output:
         ParU_Symbolic **Sym_handle,  // output, symbolic analysis
         // control:
-        ParU_Control *Control);
+        ParU_C_Control *Control);
 
 //------------------------------------------------------------------------------
 // ParU_Factorize: Numeric factorization is done in this routine. Scaling and
@@ -39,12 +74,12 @@ ParU_Ret ParU_C_Analyze(
 // structure is computed ParU_Analyze and is an input in this routine.
 //------------------------------------------------------------------------------
 ParU_Ret ParU_C_Factorize(
-    // input:
-    cholmod_sparse *A, ParU_Symbolic *Sym,
-    // output:
-    ParU_Numeric **Num_handle,
-    // control:
-    ParU_Control *Control);
+        // input:
+        cholmod_sparse *A, ParU_Symbolic *Sym,
+        // output:
+        ParU_Numeric **Num_handle,
+        // control:
+    ParU_C_Control *Control);
 
 //------------------------------------------------------------------------------
 //--------------------- Solve routines -----------------------------------------
@@ -58,7 +93,7 @@ ParU_Ret ParU_C_Solve_axx(
     // input/output:
     double *b,
     // control:
-    ParU_Control *Control);
+    ParU_C_Control *Control);
 //-------- Ax = b --------------------------------------------------------------
 ParU_Ret ParU_C_Solve_axb(
     // input:
@@ -66,7 +101,7 @@ ParU_Ret ParU_C_Solve_axb(
     // output
     double *x,
     // control:
-    ParU_Control *user_Control);
+    ParU_C_Control *user_Control);
 //-------- AX = B  (X is overwritten on B, multiple rhs)------------------------
 ParU_Ret ParU_C_Solve_AXX(
     // input
@@ -74,7 +109,7 @@ ParU_Ret ParU_C_Solve_AXX(
     // input/output:
     double *B,  // m(num_rows of A) x nrhs
     // control:
-    ParU_Control *Control);
+    ParU_C_Control *Control);
 //-------- AX = B  (multiple rhs)-----------------------------------------------
 ParU_Ret ParU_C_Solve_AXB(
     // input
@@ -82,7 +117,7 @@ ParU_Ret ParU_C_Solve_AXB(
     // output:
     double *X,
     // control:
-    ParU_Control *Control);
+    ParU_C_Control *Control);
 
 //------------------------------------------------------------------------------
 //-------------- computing residual --------------------------------------------
@@ -95,7 +130,7 @@ ParU_Ret ParU_C_Residual_bAx(
     // output:
     double &resid, double &anorm,
     // control:
-    ParU_Control *Control);
+    ParU_C_Control *Control);
 
 // resid = norm1(B-A*X) / norm1(A) (multiple rhs)
 ParU_Ret ParU_C_Residual_BAX(
@@ -104,13 +139,17 @@ ParU_Ret ParU_C_Residual_BAX(
     // output:
     double &resid, double &anorm,
     // control:
-    ParU_Control *Control);
+    ParU_C_Control *Control);
 
 //------------------------------------------------------------------------------
 //------------ Free routines----------------------------------------------------
 //------------------------------------------------------------------------------
-ParU_Ret ParU_C_Freenum(ParU_Numeric **Num_handle, ParU_Control *Control);
+ParU_Ret ParU_C_Freenum(ParU_Numeric **Num_handle, ParU_C_Control *Control);
 
-ParU_Ret ParU_C_Freesym(ParU_Symbolic **Sym_handle, ParU_Control *Control);
+ParU_Ret ParU_C_Freesym(ParU_Symbolic **Sym_handle, ParU_C_Control *Control);
+
+#ifdef __cplusplus
 }
-#endif
+#endif 
+
+#endif //PARU_C_H
