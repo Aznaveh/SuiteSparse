c60a9a48ade30bee9047f47410d40b69a7adeb95
Mohsen Aznaveh <mahmoudi.mohsen@gmail.com>
2022-10-31 15:40:28 -0500

    adding the copy function and fixing the inputs. I might be able to use a part of copy function for each of the functions. but I am doing this for simplicity for now

--- ParU/Source/ParU_C.cpp
+++ ParU/Source/ParU_C.cpp
@@ -19,7 +19,32 @@ extern "C"
 // ParU_Version: 
 // print out the version
 //------------------------------------------------------------------------------
-ParU_Ret ParU_C_Version (int ver [3], char date [128]){}
+ParU_Ret ParU_C_Version (int ver [3], char date [128])
+    {return ParU_Version (ver ,date);}
+// copy the inside of the C structrue to the Cpp structure
+void  cp_control (ParU_Control *Control, ParU_C_Control *Control_C)
+{
+    Control->mem_chunk = Control_C->mem_chunk;
+
+    Control->umfpack_ordering = Control_C->umfpack_ordering;
+    Control->umfpack_strategy = Control_C->umfpack_strategy;
+    Control->umfpack_default_singleton = Control_C->umfpack_default_singleton;
+
+    Control->relaxed_amalgamation_threshold = 
+        Control_C->relaxed_amalgamation_threshold;
+
+    Control->scale = Control_C->scale;
+    Control->panel_width = Control_C->panel_width;
+    Control->paru_strategy = Control_C->paru_strategy;
+
+
+    Control->piv_toler = Control_C->piv_toler;
+    Control->diag_toler = Control_C->diag_toler;
+    Control->trivial = Control_C->trivial;
+    Control->worthwhile_dgemm = Control_C->worthwhile_dgemm;
+    Control->worthwhile_trsm = Control_C->worthwhile_trsm;
+    Control->paru_max_threads = Control_C->paru_max_threads;
+}
 //------------------------------------------------------------------------------
 // ParU_Analyze: Symbolic analysis is done in this routine. UMFPACK is called
 // here and after that some more speciallized symbolic computation is done for
@@ -30,13 +55,16 @@ ParU_Ret ParU_C_Analyze(
         // input:
         cholmod_sparse *A,  // input matrix to analyze ...
         // output:
-        ParU_C_Symbolic **Sym_handle,  // output, symbolic analysis
+        ParU_C_Symbolic **Sym_handle_C,  // output, symbolic analysis
         // control:
-        ParU_C_Control *Control)
+        ParU_C_Control *Control_C)
 { 
     //TODO: copy the inside of the Control and then 
-    // make a new symbolic object
-    // call the Cpp ParU_Analyze
+    ParU_Control Control;
+    //TODO:  make a new symbolic object
+
+    ParU_Ret info;
+    return info = ParU_Analyze(A, &Sym, &Control);
 }
 
 //------------------------------------------------------------------------------
@@ -44,13 +72,13 @@ ParU_Ret ParU_C_Analyze(
 // making Sx matrix, computing factors and permutations is here. ParU_C_Symbolic
 // structure is computed ParU_Analyze and is an input in this routine.
 //------------------------------------------------------------------------------
-ParU_Ret ParU_C_Factorize(
+ParU_Ret ParU_C_Factorize (
         // input:
-        cholmod_sparse *A, ParU_C_Symbolic *Sym,
+        cholmod_sparse *A, ParU_C_Symbolic *Sym_C,
         // output:
-        ParU_C_Numeric **Num_handle,
+        ParU_C_Numeric **Num_handle_C,
         // control:
-    ParU_C_Control *Control)
+    ParU_C_Control *Control_C)
 { 
     //TODO: copy the inside of the Control and then 
     // make a new numeric object
@@ -63,21 +91,21 @@ ParU_Ret ParU_C_Factorize(
 // In all the solve routines Num structure must come with the same Sym struct
 // that comes from ParU_Factorize
 //-------- Ax = b (x is overwritten on b)---------------------------------------
-ParU_Ret ParU_C_Solve_Axx(
+ParU_Ret ParU_C_Solve_Axx (
     // input:
-    ParU_C_Symbolic *Sym, ParU_C_Numeric *Num,
+    ParU_C_Symbolic *Sym_C, ParU_C_Numeric *Num_C,
     // input/output:
     double *b,
     // control:
-    ParU_C_Control *Control);
+    ParU_C_Control *Control_C);
 //-------- Ax = b --------------------------------------------------------------
-ParU_Ret ParU_C_Solve_Axb(
+ParU_Ret ParU_C_Solve_Axb (
     // input:
-    ParU_C_Symbolic *Sym, ParU_C_Numeric *Num, double *b,
+    ParU_C_Symbolic *Sym_C, ParU_C_Numeric *Num_C, double *b,
     // output
     double *x,
     // control:
-    ParU_C_Control *user_Control)
+    ParU_C_Control *user_Control_C)
 { 
     //TODO: copy the inside of the Control and then 
     // call the Cpp ParU_Solve
@@ -85,13 +113,13 @@ ParU_Ret ParU_C_Solve_Axb(
 
 
 //-------- AX = B  (X is overwritten on B, multiple rhs)------------------------
-ParU_Ret ParU_C_Solve_AXX(
+ParU_Ret ParU_C_Solve_AXX (
     // input
-    ParU_C_Symbolic *Sym, ParU_C_Numeric *Num, Int nrhs,
+    ParU_C_Symbolic *Sym_C, ParU_C_Numeric *Num_C, Int nrhs,
     // input/output:
     double *B,  // m(num_rows of A) x nrhs
     // control:
-    ParU_C_Control *Control)
+    ParU_C_Control *Control_C)
 { 
     //TODO: copy the inside of the Control and then 
     // call the Cpp ParU_Solve
@@ -99,13 +127,13 @@ ParU_Ret ParU_C_Solve_AXX(
 
 
 //-------- AX = B  (multiple rhs)-----------------------------------------------
-ParU_Ret ParU_C_Solve_AXB(
+ParU_Ret ParU_C_Solve_AXB (
     // input
-    ParU_C_Symbolic *Sym, ParU_C_Numeric *Num, Int nrhs, double *B,
+    ParU_C_Symbolic *Sym_C, ParU_C_Numeric *Num_C, Int nrhs, double *B,
     // output:
     double *X,
     // control:
-    ParU_C_Control *Control)
+    ParU_C_Control *Control_C)
 { 
     //TODO: copy the inside of the Control and then 
     // call the Cpp ParU_Solve
@@ -116,13 +144,13 @@ ParU_Ret ParU_C_Solve_AXB(
 //------------------------------------------------------------------------------
 // The user provide both x and b
 // resid = norm1(b-A*x) / norm1(A)
-ParU_Ret ParU_C_Residual_bAx(
+ParU_Ret ParU_C_Residual_bAx (
     // inputs:
     cholmod_sparse *A, double *x, double *b, Int m,
     // output:
     double &resid, double &anorm,
     // control:
-    ParU_C_Control *Control)
+    ParU_C_Control *Control_C)
 { 
     //TODO: copy the inside of the Control and then 
     // call the Cpp ParU_Solve
@@ -130,13 +158,13 @@ ParU_Ret ParU_C_Residual_bAx(
 
 
 // resid = norm1(B-A*X) / norm1(A) (multiple rhs)
-ParU_Ret ParU_C_Residual_BAX(
+ParU_Ret ParU_C_Residual_BAX (
     // inputs:
     cholmod_sparse *A, double *X, double *B, Int m, Int nrhs,
     // output:
     double &resid, double &anorm,
     // control:
-    ParU_C_Control *Control)
+    ParU_C_Control *Control_C)
 { 
     //TODO: copy the inside of the Control and then 
     // call the Cpp ParU_Solve
@@ -145,14 +173,16 @@ ParU_Ret ParU_C_Residual_BAX(
 //------------------------------------------------------------------------------
 //------------ Free routines----------------------------------------------------
 //------------------------------------------------------------------------------
-ParU_Ret ParU_C_Freenum(ParU_C_Numeric **Num_handle, ParU_C_Control *Control)
+ParU_Ret ParU_C_Freenum (
+        ParU_C_Numeric **Num_handle_C, ParU_C_Control *Control_C)
 { 
     //TODO: // No need to copy the inside of the Control and then 
     // free the numeric object
 }
 
-
-ParU_Ret ParU_C_Freesym(ParU_C_Symbolic **Sym_handle, ParU_C_Control *Control)
+ 
+ParU_Ret ParU_C_Freesym (
+        ParU_C_Symbolic **Sym_handle_C, ParU_C_Control *Control_C)
 { 
     //TODO: // No need to copy the inside of the Control and then 
     // free the symblic object
